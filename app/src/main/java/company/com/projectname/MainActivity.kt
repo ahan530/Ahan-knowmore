e758e673fc49d6839e013372d18a481caa417a51
ahan530 <linke0530@163.com>
2020-05-14 13:59:47 +0800

    提交代码

---
+++ app/src/main/java/company/com/projectname/MainActivity.kt
@@ -0,0 +1,17 @@
+package company.com.projectname
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Time:2020-05-14
+ * Author:ahan
+ * Description: MVVM+DataBinding+JetPack+Kotlin语言
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
